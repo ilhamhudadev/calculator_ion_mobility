850c92b2d0cda6a7892d44565bb6db043769b7a6
ilhamhuda <ilham.huda@istyle.id>
2024-06-12 14:03:14 +0700

    [UPDATE] Update Widget Test to test Widget on Main Page

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,71 +1,82 @@
-import 'package:calculator_ion_mobility/module/main/bloc/main_bloc.dart';
 import 'package:calculator_ion_mobility/module/main/pages/main_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:calculator_ion_mobility/module/main/bloc/main_bloc.dart';
+import 'package:calculator_ion_mobility/module/main/widget/button/action_button.dart';
+import 'package:calculator_ion_mobility/module/main/widget/button/number_button.dart';
+import 'package:calculator_ion_mobility/module/main/widget/header/header_section.dart';
+import 'package:calculator_ion_mobility/module/main/widget/result/result_section.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  testWidgets('displays initial input and result', (WidgetTester tester) async {
-    // Pump the widget tree
-    await tester.pumpWidget(
-      BlocProvider(
-        create: (context) => MainBloc(), // Create a MainBloc instance
-        child: const MaterialApp(home: MainPage()), // Build the MainPage widget
-      ),
-    );
+  group('MainPage Widget Test', () {
+    late MainBloc mainBloc;
 
-    // Verify if '0' is displayed twice (for initial input and result)
-    expect(find.text('0'), findsNWidgets(2));
-  });
+    setUp(() {
+      mainBloc = MainBloc();
+    });
 
-  // Test to check if input and result are updated correctly when buttons are pressed
-  testWidgets('updates input and result when buttons are pressed',
-      (WidgetTester tester) async {
-    // Pump the widget tree
-    await tester.pumpWidget(
-      BlocProvider(
-        create: (context) => MainBloc(), // Create a MainBloc instance
-        child: const MaterialApp(home: MainPage()), // Build the MainPage widget
-      ),
-    );
+    testWidgets('renders MainPage and interacts with widgets',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: BlocProvider<MainBloc>(
+            create: (_) => mainBloc,
+            child: const MainPage(),
+          ),
+        ),
+      );
 
-    // Simulate button presses
-    await tester.tap(find.text('1'));
-    await tester.tap(find.text('+'));
-    await tester.tap(find.text('1'));
-    await tester.tap(find.text('='));
-    await tester.pump();
+      // Verify the presence of HeaderSection
+      expect(find.byType(HeaderSection), findsOneWidget);
 
-    // Verify if input expression '1+1' and result '2' are displayed
-    expect(find.text('1+1'), findsOneWidget);
-    expect(find.text('2'), findsOneWidget);
-  });
+      // Verify the presence of ResultSection
+      expect(find.byType(ResultSection), findsOneWidget);
 
-  // Test to check if history is displayed correctly
-  testWidgets('displays history correctly', (WidgetTester tester) async {
-    // Pump the widget tree
-    await tester.pumpWidget(
-      BlocProvider(
-        create: (context) => MainBloc(), // Create a MainBloc instance
-        child: const MaterialApp(home: MainPage()), // Build the MainPage widget
-      ),
-    );
+      // Verify the presence of NumberButtons (1, 2, 3)
+      expect(find.widgetWithText(NumberButton, '1'), findsOneWidget);
+      expect(find.widgetWithText(NumberButton, '2'), findsOneWidget);
+      expect(find.widgetWithText(NumberButton, '3'), findsOneWidget);
 
-    // Simulate button presses for two calculations
-    await tester.tap(find.text('2'));
-    await tester.tap(find.text('+'));
-    await tester.tap(find.text('3'));
-    await tester.tap(find.text('='));
-    await tester.pump();
+      // Tap number buttons and verify interaction
+      await tester.tap(find.widgetWithText(NumberButton, '1'));
+      await tester.tap(find.widgetWithText(NumberButton, '2'));
+      await tester.tap(find.widgetWithText(NumberButton, '3'));
+      await tester.pump();
 
-    await tester.tap(find.text('4'));
-    await tester.tap(find.text('*'));
-    await tester.tap(find.text('2'));
-    await tester.tap(find.text('='));
-    await tester.pump();
+      // Tap ActionButton for 'history_outlined' and open drawer
+      await tester.tap(find.byIcon(Icons.history_outlined));
+      await tester.pump();
 
-    // Verify if history entries are displayed correctly
-    expect(find.text('2+3 = 5'), findsOneWidget);
-    expect(find.text('4*2 = 8'), findsOneWidget);
+      // Verify if Drawer is opened
+      expect(find.byType(Drawer), findsOneWidget);
+
+      // Close the drawer
+      await tester.dragFrom(const Offset(300, 300), const Offset(-300, 0));
+      await tester.pump();
+
+      // Tap ActionButton for 'backspace'
+      await tester.tap(find.byIcon(Icons.backspace));
+      await tester.pump();
+
+      // Tap ActionButton for '='
+      await tester.tap(find.widgetWithText(ActionButton, '='));
+      await tester.pump();
+
+      // Tap ActionButton for 'C' (Clear)
+      await tester.tap(find.widgetWithText(ActionButton, 'C'));
+      await tester.pump();
+
+      // Tap arithmetic operator buttons
+      await tester.tap(find.widgetWithText(ActionButton, '\u00F7'));
+      await tester.tap(find.widgetWithText(ActionButton, '-'));
+      await tester.tap(find.widgetWithText(ActionButton, 'x'));
+      await tester.tap(find.widgetWithText(ActionButton, '+'));
+      await tester.pump(); // Rebuild the widget after the state has changed
+    });
+
+    tearDown(() {
+      mainBloc.close();
+    });
   });
 }
